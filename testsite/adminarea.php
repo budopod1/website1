fbf770844497f52b96df94c6dc03f55bcb645a5d
budopod1 <martin.e.staab@gmail.com>
2019-06-14 14:29:43 -0700

    Added note in admin area

--- testsite/adminarea.php
+++ testsite/adminarea.php
@@ -5,9 +5,9 @@ include "adminsession.php";
 #Include connection file
 include "conn.php";
 
-if (isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     #Get the id
-    $id=$_POST['id'];
+    $id = $_POST['id'];
 
     #Update data in data base
     $sql = "UPDATE bugs SET resolved = 1 WHERE id=$id";
@@ -31,12 +31,13 @@ if (isset($_POST['register'])) {
         $sql = "INSERT INTO `admin` (username, `password`) VALUES ('$username', '$password')";
         $connection->exec($sql);
     } else {
-        $err ="Passwords don't match";
+        $err = "Passwords don't match";
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -44,93 +45,95 @@ if (isset($_POST['register'])) {
     <title>Admin Control Panel</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
         <?php include "nav.php" ?>
         <h1>Admin Control Area</h1>
-        <h2>Welcome <?php echo $_SESSION['username']?></h1>
-        <br>
-        <?php if (isset($err)){
-            echo "<h4>".$err."</h4>";
-        }?>
-        <br>
-        <details>
-            <summary>Add new admin</summary>
-            <div class="row pt-3">
-                <div class="col"></div>
-                <div class="col border border-success p-3 pl-5 pr-5">
-                    <h1>Register</h1>
-                    <form action="" method="post">
-                        <div class="form-group">
-                            <label for="username">Enter Username:</label>
-                            <input type="text" name="username" id="username" placeholder="username">
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Enter Password:</label>
-                            <input type="password" name="password" id="password" placeholder="password">
-                        </div>
-                        <div class="form-group">
-                            <label for="password2">Enter Password (Comfirm):</label>
-                            <input type="password" name="password2" id="password2" placeholder="password">
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="register">Register</button>
-                    </form>
+        <h2>Welcome <?php echo $_SESSION['username'] ?></h1>
+            <br>
+            <?php if (isset($err)) {
+                echo "<h4>" . $err . "</h4>";
+            } ?>
+            <br>
+            <details class="mb-3">
+                <summary>Add new admin</summary>
+                <div class="row pt-3">
+                    <div class="col"></div>
+                    <div class="col border border-success p-3 pl-5 pr-5">
+                        <h1>Register</h1>
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="username">Enter Username:</label>
+                                <input type="text" name="username" id="username" placeholder="username">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Enter Password:</label>
+                                <input type="password" name="password" id="password" placeholder="password">
+                            </div>
+                            <div class="form-group">
+                                <label for="password2">Enter Password (Comfirm):</label>
+                                <input type="password" name="password2" id="password2" placeholder="password">
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="register">Register</button>
+                        </form>
+                    </div>
+                    <div class="col"></div>
                 </div>
-                <div class="col"></div>
-            </div>
-        </details>
-        <br>
-        <div class="row">
-            <div class="col">
-                <p><b>Title</b></p>
-            </div>
-            <div class="col">
-                <p><b>Content</b></p>
-            </div>
-            <div class="col">
-                <p><b>Resolved</b></p>
-            </div>
-            <div class="col">
-                <p><b>Resolve</b></p>
-            </div>
-        </div>
-        <?php
-        if ($bugs && $st->rowCount() > 0) {
-            foreach ($bugs as $bug) { ?>
-                <div class="row mt-3">
-                    <div class="col">
-                        <?php
-                        echo "<a href=\"bug.php?bugid=".$bug['id']."\">".$bug['title']."</a>"
-                        ?>
-                    </div>
-                    <div class="col">
-                        <?php
-                        echo substr($bug['content'], 0, 20)."...";
-                        ?>
-                    </div>
-                    <div class="col">
-                        <?php
-                        if ($bug["resolved"] == 0){
-                            echo "No";
-                        } else {
-                            echo "Yes";
-                        }
-                        ?>
-                    </div>
-                    <div class="col">
-                        <?php if ($bug["resolved"] == 0){?>
-                            <form action="" method="post">
-                                <input type="hidden" name="id" value="<?php echo $bug["id"] ?>">
-                                <button type="submit" name="submit" class="btn btn-success">Resolve</button>
-                            </form>
-                        <?php } ?>
-                    </div>
+            </details>
+            <small>(Resolve button will disappear if already resolved)</small>
+            <div class="row mt-3">
+                <div class="col">
+                    <p><b>Title</b></p>
                 </div>
+                <div class="col">
+                    <p><b>Content</b></p>
+                </div>
+                <div class="col">
+                    <p><b>Resolved</b></p>
+                </div>
+                <div class="col">
+                    <p><b>Resolve</b></p>
+                </div>
+            </div>
             <?php
+            if ($bugs && $st->rowCount() > 0) {
+                foreach ($bugs as $bug) { ?>
+                    <div class="row mt-3">
+                        <div class="col">
+                            <?php
+                            echo "<a href=\"bug.php?bugid=" . $bug['id'] . "\">" . $bug['title'] . "</a>"
+                            ?>
+                        </div>
+                        <div class="col">
+                            <?php
+                            echo substr($bug['content'], 0, 20) . "...";
+                            ?>
+                        </div>
+                        <div class="col">
+                            <?php
+                            if ($bug["resolved"] == 0) {
+                                echo "No";
+                            } else {
+                                echo "Yes";
+                            }
+                            ?>
+                        </div>
+                        <div class="col">
+                            <?php if ($bug["resolved"] == 0) { ?>
+                                <form action="" method="post">
+                                    <input type="hidden" name="id" value="<?php echo $bug["id"] ?>">
+                                    <button type="submit" name="submit" class="btn btn-success">Resolve</button>
+                                </form>
+                            <?php } ?>
+                        </div>
+                    </div>
+                <?php
             }
         }
         ?>
-        <?php include "footer.php"; ?>
+            <?php include "footer.php"; ?>
     </div>
 </body>
+
 </html>
